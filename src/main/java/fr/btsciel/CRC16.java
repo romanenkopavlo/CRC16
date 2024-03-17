0a1bab8b1257b8a9f11b43567157f7c5345f7af0
utopiaforever <pavlo.romanenko@lyceeastier.com>
2024-03-17 13:38:53 +0100

    CRC16 est affiche

--- src/main/java/fr/btsciel/CRC16.java
+++ src/main/java/fr/btsciel/CRC16.java
@@ -3,24 +3,34 @@ package fr.btsciel;
 public class CRC16 {
     public static int stdPoly = 0xA001;
     public static int initialValue = 0xffff;
+    public static int crc16;
     public static void main(String[] args) {
-        System.out.println(calculCRC16(formatage("0103"), initialValue, stdPoly));
+        crc16 = calculCRC16(formatage("0103"), initialValue, stdPoly);
+        byte [] trame = formatage("0103");
+        for (int i = 0; i < trame.length; i++) {
+            if (i != trame.length - 1) {
+                System.out.print("|" + trame[i] + "|");
+            } else {
+                System.out.print(trame[i] + "| ");
+            }
+
+        }
+        System.out.print("Le CRC16 = " + crc16 + " (0x" + Integer.toHexString(crc16) + ")");
     }
     public static int calculCRC16(byte [] octets, int valeurInitiale, int polynomme) {
         byte byteToTreat;
         for (int i = 0; i < octets.length; i++) {
-            byteToTreat = octets[i];
-            initialValue += byteToTreat;
+            byteToTreat = (byte) (octets[i] & 0xff);
+            valeurInitiale = valeurInitiale ^ byteToTreat;
             for (int j = 0; j < 8; j++) {
-                if (initialValue % 2 == 0) {
-                    initialValue = initialValue / 2;
+                if (valeurInitiale % 2 == 0) {
+                    valeurInitiale = valeurInitiale / 2;
                 } else {
-                    initialValue = (initialValue / 2) ^ stdPoly;
+                    valeurInitiale = (valeurInitiale / 2) ^ polynomme;
                 }
             }
         }
-
-        return initialValue;
+        return valeurInitiale;
     }
     public static byte[] hexStringEnByteArray(String message) {
         int size = message.length();
