1929cac64d051e8d15b5e63bd3fa78f41af91511
utopiaforever <pavlo.romanenko@lyceeastier.com>
2024-03-17 14:20:54 +0100

    CRC16 est calcule

--- src/main/java/fr/btsciel/CRC16.java
+++ src/main/java/fr/btsciel/CRC16.java
@@ -3,20 +3,34 @@ package fr.btsciel;
 public class CRC16 {
     public static int stdPoly = 0xA001;
     public static int initialValue = 0xffff;
+    public static String hexaRegex = "([0-9a-fA-F][0-9a-fA-F]|[a-fA-F][0-9a-fA-F])+";
+    public static String chaineHexa;
     public static int crc16;
-    public static void main(String[] args) {
-        crc16 = calculCRC16(formatage("0103"), initialValue, stdPoly);
-        byte [] trame = formatage("0103");
-        for (int i = 0; i < trame.length; i++) {
-            if (i != trame.length - 1) {
-                System.out.print("|" + trame[i] + "|");
-            } else {
-                System.out.print(trame[i] + "| ");
-            }
 
+
+    public static void main(String[] args) {
+        System.out.print("Chaine d'octets en hexa pour le calcul du CRC16: ");
+        chaineHexa = new String(In.readString());
+
+        if (formatage(chaineHexa) != null) {
+            crc16 = calculCRC16(formatage(chaineHexa), initialValue, stdPoly);
+            byte [] trame = formatage(chaineHexa);
+            for (int i = 0; i < trame.length; i++) {
+                if (i != trame.length - 1 && i % 2 == 0) {
+                    System.out.print("|" + trame[i] + "|");
+                } else if (i != trame.length - 1 && i % 2 != 0){
+                    System.out.print(trame[i]);
+                } else {
+                    System.out.print(trame[i] + "| ");
+                }
+            }
+            System.out.print("Le CRC16 = " + crc16 + " (0x" + Integer.toHexString(crc16) + ")");
+        } else {
+            System.out.print("Un mauvais format de saisie");
         }
-        System.out.print("Le CRC16 = " + crc16 + " (0x" + Integer.toHexString(crc16) + ")");
     }
+
+
     public static int calculCRC16(byte [] octets, int valeurInitiale, int polynomme) {
         byte byteToTreat;
         for (int i = 0; i < octets.length; i++) {
@@ -42,10 +56,13 @@ public class CRC16 {
         return data;
     }
     public static byte[] formatage(String trame) {
-        int size = trame.length();
-        if (size % 2 != 0) {
-            trame += "0";
+        if (trame.matches(hexaRegex)) {
+            int size = trame.length();
+            if (size % 2 != 0) {
+                trame += "0";
+            }
+            return hexStringEnByteArray(trame);
         }
-        return hexStringEnByteArray(trame);
+        return null;
     }
 }
